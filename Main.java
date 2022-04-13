e6194921263f686ac7bdf162a7843acf652f2685
Ms. Kati Kati <90414307+MsKati@users.noreply.github.com>
2022-04-13 16:51:10 +0000

    ;cv/cx.cs/A>CaC>a.dxs/A>C ./sc,saml a,s,z/a.mnv ds.sx?<CMV XFD,V.C?<c mbc.vc .x,mzv ,cx?>< sz.  }

--- Main.java
+++ Main.java
@@ -1,5 +1,4 @@
 class Main {
   public static void main(String[] args) {
-    System.out.println("This is my upcoming piano project");
-  }
+    System.out.println("This is my upcoming piano project");cv/cx.cs/A>CaC>a.dxs/A>C ./sc,saml a,s,z/a.mnv ds.sx?<CMV XFD,V.C?<c mbc.vc .x,mzv ,cx?>< sz.  }
 }
